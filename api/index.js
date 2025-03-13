a48cde833bf1b0e695fee94f01ba335594f4e7df
peachey13 <beccapeachey13@gmail.com>
2025-03-13 13:24:31 -0600

    Create index.js

---
+++ api/index.js
@@ -0,0 +1,8 @@
+module.exports = async (req, res) => {
+  const apiKey = 'UsFenY7QpHuDKyYG961seFUd8xwHFC8N';
+  const listingId = '613014927246709169';
+  const startDate = '2025-03-01';
+  const endDate = '2025-12-31';
+  try {
+    const response = await fetch(`https://api.pricelabs.co/listings/${listingId}/pricing?start_date=${startDate}&end_date=${endDate}`, {
+      headers: { 'Authorization': `Bearer ${api...
