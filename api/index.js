4a6fb20a4bb4f30650f0b2f1608cd272806dfb6a
peachey13 <beccapeachey13@gmail.com>
2025-03-13 14:00:10 -0600

    Update index.js

--- api/index.js
+++ api/index.js
@@ -2,12 +2,19 @@ const fetch = require('node-fetch');
 
 module.exports = async (req, res) => {
   const apiKey = 'UsFenY7QpHuDKyYG961seFUd8xwHFC8N';
-  const listingId = '613014927246709169';
-  const startDate = '2025-03-01';
-  const endDate = '2025-12-31';
+  const body = {
+    "listing_ids": ["613014927246709169"],
+    "start_date": "2025-03-01",
+    "end_date": "2025-12-31"
+  };
   try {
-    const response = await fetch(`https://api.pricelabs.co/listings/${listingId}/pricing?start_date=${startDate}&end_date=${endDate}`, {
-      headers: { 'Authorization': `Bearer ${apiKey}` }
+    const response = await fetch('https://api.pricelabs.co/pricesForListings', {
+      method: 'POST',
+      headers: {
+        'Authorization': `Bearer ${apiKey}`,
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(body)
     });
     if (!response.ok) throw new Error(`HTTP error! Status: ${response.status}`);
     const data = await response.json();
